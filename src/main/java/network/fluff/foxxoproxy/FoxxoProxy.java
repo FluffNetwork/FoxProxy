16856f9618f65eed1c36b4bca944670486315c6b
Checkium Folf <checkium@gmail.com>
2020-02-25 03:02:16 +0000

    Add license and build scripts

--- src/main/java/network/fluff/foxxoproxy/FoxxoProxy.java
+++ src/main/java/network/fluff/foxxoproxy/FoxxoProxy.java
@@ -1,4 +1,16 @@
 package network.fluff.foxxoproxy;
 
+import raknetserver.RakNetServer;
+
 public class FoxxoProxy {
+
+    private RakNetServer server;
+
+    public FoxxoProxy() {
+
+    }
+
+    public static void main(String[] args) {
+        new FoxxoProxy();
+    }
 }
